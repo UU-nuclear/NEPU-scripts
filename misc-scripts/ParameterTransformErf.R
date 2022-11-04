7930187cdad239d679ad1ad3cacad43620206bb9
alfgook <alf.gook@physics.uu.se>
2022-11-04 14:08:21 +0100

    before run

---
+++ misc-scripts/ParameterTransformErf.R
@@ -0,0 +1,56 @@
+# transformations used to restrict TALYS parameters 
+# to lie in a certain interval. The logistic function
+# is used as transformation.
+# Let xt = f(x) with the transformed parameter xt and the original parameter x 
+# Parameters:
+#  x0    ... the turning point where f(x0) = x0, i.e. transformed parameter 
+#            equals untransformed parameter 
+#  x_min ... is the lower limit for the original parameters 
+#  x_max ... is the upper limit for the original parameters 
+# the rate of change is adapted so that df/dx = 1 at x = x0
+#
+# parameterTransform expects vectors x0, x_min, x_max, each of the same
+# length as the number of parameters holding the limits of the corresponding
+# parameter
+
+erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
+
+erfinv <- function (x) qnorm((1 + x)/2)/sqrt(2)
+
+transFunc <- function(x, x0, delta) {
+  x0 + delta*(2*pnorm((x-x0)/delta) - 1)
+}
+
+inv_transFunc <- function(z, x0, delta) {
+  x0 + delta*qnorm(0.5*(1+(z-x0)/delta))
+}
+
+jac_transFunc <- function(x, x0, delta) {
+  x <- 2*delta*dnorm(x,x0,delta)
+  diag(x = x, nrow = length(x))
+}
+
+parameterTransform <- function(x0, delta) {
+
+  fun <- function(x) {
+    if(is.vector(x)) {
+      transFunc(x, x0, delta)
+    } else if(is.matrix(x)) {
+      apply(x,2,transFunc,x0=x0,delta)
+    }
+  }
+
+  invfun <- function(z) {
+    if(is.vector(z)) {
+      inv_transFunc(z, x0, delta)
+    } else if(is.matrix(z)) {
+      apply(z,2,inv_transFunc,x0=x0,delta)
+    }
+  }
+
+  jac <- function(x) {
+    jac_transFunc(x, x0, delta)
+  }
+
+  list(fun = fun, jac = jac, invfun = invfun)
+}
---
+++ script/06_tune_endep_hyperpars_with_prior.R
@@ -0,0 +1,215 @@
+#
+# DESCRIPTION OF STEP
+#
+# tune the hyperparameters of the Gaussian
+# processes attached to the energy-dependent
+# TALYS parameters using maximunm likelihood
+# optimization.
+#
+# This script is the same as 06_tune_endep_hyperpars_fixed_sigma.R
+# with the difference that the upper limit for sigma is set by the prior
+# uncertainty of the corresponding energy independent parameter
+
+#################################################
+#       SCRIPT Setup
+##################################################
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==1) {
+  print(paste0("Setting as config file: ", args[1]))
+  source(args[1])
+}
+
+#################################################
+#       SCRIPT PARAMETERS
+##################################################
+
+scriptnr <- 6L
+overwrite <- FALSE
+
+##################################################
+#       OUTPUT FROM PREVIOUS STEPS
+##################################################
+
+subents <- read_object(1, "subents")
+refParamDt <- read_object(2, "refParamDt")
+extNeedsDt <- read_object(2, "extNeedsDt")
+modList <- read_object(3, "modList")
+expDt <- read_object(3, "expDt")
+updSysDt <- read_object(4, "updSysDt")
+fullSensDt <- read_object(5, "fullSensDt") 
+
+source("misc-scripts/MLOwithPrior.R")
+library(parallel)
+##################################################
+#       START OF SCRIPT
+##################################################
+
+
+
+print("-----------------------------------------------------")
+print("----------------------script 06----------------------")
+print("-----------------------------------------------------")
+
+# define objects to be returned
+outputObjectNames <- c("optExpDt", "optSysDt", "optGpDt","sysCompHandler")
+check_output_objects(scriptnr, outputObjectNames)
+
+# prepare the data table with systematic components
+# this comprises both systematic experimental 
+# errors and the model parameters
+
+talysHandler <- createSysCompModelHandler()
+talysHandler$setRef(extNeedsDt, fullSensDt, refParamDt,
+                    exforHandler, c(subents, modList$SUBENT))
+talysHandler$setPrior(refParamDt)
+modelSysDt <- talysHandler$createSysDt()
+
+setkey(updSysDt, IDX)
+curSysDt <- rbind(updSysDt, modelSysDt, fill=TRUE)
+curSysDt <- curSysDt[!grepl("REACEXP-", EXPID)]
+curSysDt[, IDX := seq_len(.N)]
+curSysDt[, ADJUSTABLE := FALSE]
+
+# set up Gaussian processes for energy dependent parameters
+gpHandler <- createSysCompGPHandler()
+parnames_endep <- unique(curSysDt[ERRTYPE == "talyspar_endep", EXPID])
+for (curParname in parnames_endep) {
+    gpHandler$addGP(curParname, curSysDt[EXPID==curParname,UNC][1], 50, 1e-4)
+}
+
+# create a mapping matrix from the model output
+# to the grid of the experimental data
+Sexp <- exforHandler$getJac(expDt, extNeedsDt, subents) 
+
+# prepare the experimental data
+curExpDt <- copy(expDt)
+setkey(curExpDt, IDX)
+setkey(extNeedsDt, IDX)
+# REFDATA contains the cross sections of the reference model
+# calculation. It serves as the expansion point in the
+# Taylor approximation of the model entering the Bayesian
+# update
+curExpDt[, REFDATA := as.vector(Sexp %*% extNeedsDt$V1)]
+curExpDt[, ADJUSTABLE := FALSE]
+
+gpDt <- gpHandler$createGPDt()
+gpHandler$updateSysDt(curSysDt)
+
+# define which GP hyperparameters are adjustable
+gpDt[, ADJUSTABLE := PARNAME %in% c("len","sigma")]
+gpDt[, IDX := seq_len(.N)]
+
+# initialize handler for systematic experimental uncertainties
+# TODO: It should not be needed to define the systematic error
+#       components during the setup. The category (column name)
+#       should be enough.
+normHandler <- createSysCompNormHandler("DATAREF")
+tmpDt <- updSysDt[grepl("EXPID-", EXPID)]
+expIds <- sub("EXPID-", "", tmpDt$EXPID)
+normHandler$addSysUnc("EXPID", expIds, 0, tmpDt$UNC,
+                      rel = (tmpDt$ERRTYPE == "sys-rel"))
+
+# create global handler and register the individual handlers
+sysCompHandler <- createSysCompHandler()
+sysCompHandler$addHandler(normHandler)
+sysCompHandler$addHandler(talysHandler)
+sysCompHandler$addGPHandler(gpHandler)
+
+# setup optimization specification
+
+optfuns <- createMLOptimFuns()
+optfuns$setDts(curExpDt, curSysDt, gpDt,
+               sysCompHandler = sysCompHandler)
+
+# create a tempory data table with the uncertainty for each parameter
+tmp <- curSysDt[ERRTYPE=="talyspar_endep"]
+tmp[,DIDX:=NULL]
+tmp[,IDX:=NULL]
+tmp[,EN:=NULL]
+tmp[,ORIGIDX:=NULL]
+tmp <- unique(tmp)
+stopifnot(gpDt[PARNAME=="sigma"]$EXPID == tmp$EXPID)
+# They should have the same order, but should make some reordering of tmp
+
+
+
+# define the hyperparameter limits
+setkey(gpDt, IDX)
+lowerLims <- rep(NA_real_, nrow(gpDt))
+upperLims <- rep(NA_real_, nrow(gpDt))
+lowerLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- 0.25*tmp$UNC
+lowerLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- 3
+upperLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- 2*tmp$UNC
+upperLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- 200
+
+# initial configuration
+initPars <- rep(NA_real_, nrow(gpDt))
+initPars[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- 0.5*tmp$UNC
+initPars[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- 10
+
+# prior on the hyper-parameter
+priorExpectation <- rep(NA_real_, nrow(gpDt))
+priorExpectation[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- 0.5*tmp$UNC
+priorExpectation[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- 50
+
+priorUnc <- rep(NA_real_, nrow(gpDt))
+priorUnc[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- 0.5*tmp$UNC
+priorUnc[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- 50
+
+#initPars <- upperLims
+lowerLims <- lowerLims[gpDt$ADJUSTABLE]
+upperLims <- upperLims[gpDt$ADJUSTABLE]
+initPars <- initPars[gpDt$ADJUSTABLE]
+priorExpectation <- priorExpectation[gpDt$ADJUSTABLE]
+priorUnc <- priorUnc[gpDt$ADJUSTABLE]
+
+# we assume no prior correlations
+priorCovMat <- Diagonal(length(priorUnc),x=priorUnc^2)
+
+
+# set the prior
+optfuns$setPrior(priorExpectation,priorCovMat)
+
+#########################
+min_sigma <- max(0.1*tmp$UNC)
+max_sigma <- min(2*tmp$UNC)
+min_length <- 6
+max_length <- 100
+
+n_sigmas <- length(lowerLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"])
+n_lengths <- length(lowerLims[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"])
+
+n_adjustable_hyper_pars <- n_sigmas + n_lengths
+hyper_par_vector <- rep(0, n_adjustable_hyper_pars)
+
+n_samples <- 32*10
+sigma_samples <- runif(n_sigmas*n_samples,min_sigma,max_sigma)
+length_samples <- runif(n_lengths*n_samples,min_length,max_length)
+
+hyper_par_samples <- rep(NA_real_, nrow(gpDt)*n_samples)
+hyper_par_samples[gpDt$ADJUSTABLE & gpDt$PARNAME == "sigma"] <- sigma_samples
+hyper_par_samples[gpDt$ADJUSTABLE & gpDt$PARNAME == "len"] <- length_samples
+hyper_par_samples <- hyper_par_samples[!is.na(hyper_par_samples)] # removing the ones that are not adjustable
+hyper_par_samples <- matrix(hyper_par_samples,ncol=n_samples)
+
+nCores <- detectCores(all.tests = FALSE, logical = TRUE)
+results <- mclapply(X=as.data.frame(hyper_par_samples),FUN=optim, fn=optfuns$logPost, gr=optfuns$gradLogPost,
+      method = "L-BFGS-B", lower = lowerLims, upper = upperLims, control = list(fnscale = -1),
+      mc.cores = getOption("mc.cores", nCores))
+
+results <- simplify2array(results)
+best_result <- results[,unlist(results["value",]) == max(unlist(results["value",]))][,1]
+
+newDts <- optfuns$getModifiedDts(best_result$par)
+#
+optExpDt <- newDts$expDt
+optSysDt <- newDts$sysDt
+optGpDt <- newDts$gpDt
+
+# save the needed files for reference
+save_output_objects(scriptnr, outputObjectNames, overwrite)
+
+print("---- optimised endep hyperparameters -----")
+print(optGpDt[ADJUSTABLE==TRUE],topn=nrow(optGpDt[ADJUSTABLE==TRUE]))
