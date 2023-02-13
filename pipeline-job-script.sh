7c997ff85bb1fb04b110c7de36d1997ec7f2d3e3
alfgook <alg.gook@physics.uu.se>
2023-02-13 16:18:54 +0100

    work in progress

--- pipeline-job-script.sh
+++ pipeline-job-script.sh
@@ -10,10 +10,10 @@
 #SBATCH -t 40:00
 #SBATCH -J talysTemp
 
-BASE_DIR = /proj/naiss2023-22-58
-SIF_FILE = $BASE_DIR/ND-eval-pipeline/NDeval-pipeline-no-stdout-redirect.sif
-SCRIPT_DIR = $BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
-CONFIG_FILE = $BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-test.R
+BASE_DIR=/proj/naiss2023-22-58
+SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-no-stdout-redirect.sif
+SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
+CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-test.R
 
 apptainer exec --bind $BASE_DIR $SIF_FILE mpirun -np 1 Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
 apptainer exec --bind $BASE_DIR $SIF_FILE mpirun -np 1 Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties.R $CONFIG_FILE
