1c993d01060beacadb1a5f77e55188945bf3d5ac
alfgook <alg.gook@physics.uu.se>
2023-02-13 16:30:20 +0100

    work in progress

--- pipeline-job-script.sh
+++ pipeline-job-script.sh
@@ -13,7 +13,7 @@
 BASE_DIR=/proj/naiss2023-22-58
 SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-no-stdout-redirect.sif
 SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
-CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-test.R
+CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func.R
 
 apptainer exec --bind $BASE_DIR $SIF_FILE mpirun -np 1 Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties.R $CONFIG_FILE
