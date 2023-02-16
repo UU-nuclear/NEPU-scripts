c428bde60d686c12991c794eb951670a711a3015
alfgook <alg.gook@physics.uu.se>
2023-02-16 16:06:59 +0100

    work in progress

--- step05-job-script.sh
+++ step05-job-script.sh
@@ -1,22 +1,19 @@
 #!/bin/bash -l
 
-# this job script is specifical for step 02 of the pipeline that does not use
-# more than two cores:
-# one for the main thread and one worker to do the talys calculation
-# so I will run it on a single node requsting two cores
+# this job script is specifical for step 05 of the pipeline
 
 #SBATCH -A naiss2023-22-58
 
 # I will try to ask for 2 nodes to start with
-#SBATCH -p node -N 2
-#SBATCH -t 15:00
+#SBATCH -p node -N 5
+#SBATCH -t 01:00:00
 #SBATCH -J talysTemp
 
 BASE_DIR=/proj/naiss2023-22-58
 SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-rackham-with-stdout-redirect.sif
 SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
-#CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func.R
-CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-test.R
+CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func.R
+#CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-test.R
 
 module load openmpi/4.0.2
-mpirun -np 40 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/05_create_reference_jacobian.R $CONFIG_FILE
+mpirun -np 100 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/05_create_reference_jacobian.R $CONFIG_FILE
