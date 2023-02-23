5dbdeed0b799d3fcb33d5c511ad1716b952879ae
alfgook <alg.gook@physics.uu.se>
2023-02-23 16:43:36 +0100

    add script to submit a series of slurm jobs

--- pipeline-job-01.sh
+++ pipeline-job-01.sh
@@ -6,8 +6,8 @@
 # so I will run it on a single node requsting two cores
 
 #SBATCH -A naiss2023-22-58
-#SBATCH -p core -n 2
-#SBATCH -t 15:00
+#SBATCH -p core -n 1
+#SBATCH -t 5:00
 #SBATCH -J job-01-ND-pipeline
 
 BASE_DIR=/proj/naiss2023-22-58
@@ -15,7 +15,10 @@ SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-rackham-with-stdout-redirect
 SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
 CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func-try2.R
 
-apptainer exec --bind $BASE_DIR $SIF_FILE mpirun -np 1 Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
+module load openmpi/4.0.2
+mpirun -np 1 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
+
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties.R $CONFIG_FILE
+
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/04_tune_experimental_uncertainties.R $CONFIG_FILE
 
