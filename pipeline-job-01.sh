807e1abf0674cdf86eee98d2160eb2599e942656
alfgook <alg.gook@physics.uu.se>
2023-02-23 16:46:59 +0100

    add script to submit a series of slurm jobs

--- pipeline-job-01.sh
+++ pipeline-job-01.sh
@@ -6,7 +6,7 @@
 # so I will run it on a single node requsting two cores
 
 #SBATCH -A naiss2023-22-58
-#SBATCH -p core -n 1
+#SBATCH -p core -n 2
 #SBATCH -t 5:00
 #SBATCH -J job-01-ND-pipeline
 
@@ -16,7 +16,7 @@ SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
 CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func-try2.R
 
 module load openmpi/4.0.2
-mpirun -np 1 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
+mpirun -np 2 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/02_create_reference_calculation.R $CONFIG_FILE
 
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties.R $CONFIG_FILE
 
