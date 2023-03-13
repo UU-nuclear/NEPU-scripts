7f9eedc2d72bbe54ce7635571fe7884ac1196284
alfgook <alg.gook@physics.uu.se>
2023-03-13 15:19:03 +0100

    submit script for step5

---
+++ script-replace-data/job02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# this job script is specifical for step 05 of the pipeline
+
+#SBATCH -A naiss2023-22-58
+
+#SBATCH -p node -N 5
+#SBATCH -t 01:20:00
+#SBATCH -J job-02-ND-pipeline
+
+BASE_DIR=/proj/naiss2023-22-58
+SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-rackham-with-stdout-redirect-new.sif
+SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-replace-data
+CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/test-replace-data.R
+
+module load openmpi/4.0.2
+mpirun -np 100 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/05_create_reference_jacobian.R $CONFIG_FILE
