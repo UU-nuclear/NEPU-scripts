8a1c084e3aff84c63f87f863ffe510598d958dfb
alfgook <alg.gook@physics.uu.se>
2023-03-16 17:24:08 +0100

    modified step 06 for data replacement

--- script-replace-data/job03.sh
+++ script-replace-data/job03.sh
@@ -13,6 +13,7 @@ SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-rackham-with-stdout-redirect
 SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-replace-data
 CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/test-replace-data.R
 
+apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/05_select_parameters.R $CONFIG_FILE
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/06_tune_endep_hyperpars_mod_cov_func.R $CONFIG_FILE
 
 # for the case in config-Cr52-mod-cov-func.R the optimization took about 1 h on 20 cores, so 20 corehours
