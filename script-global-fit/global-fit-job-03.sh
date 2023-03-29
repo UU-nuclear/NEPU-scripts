548130f9c8c51392b1729f3e7914a4bafaae38c9
alfgook <alg.gook@physics.uu.se>
2023-03-29 15:49:15 +0200

    scripts for global fit

--- script-global-fit/global-fit-job-03.sh
+++ script-global-fit/global-fit-job-03.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-global-fit
 CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-global-fit.R
 
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/05_select_parameters.R $CONFIG_FILE
-apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/06_tune_endep_hyperpars_mod_cov_func.R $CONFIG_FILE
+#apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/06_tune_endep_hyperpars_mod_cov_func.R $CONFIG_FILE
 
 # for the case in config-Cr52-mod-cov-func.R the optimization took about 1 h on 20 cores, so 20 corehours
 
