77b9f1147c88a90cbaa38341d13eaf0c356a1037
alfgook <alg.gook@physics.uu.se>
2023-04-17 14:13:58 +0200

    added Fe56 config file

---
+++ Fe56-job-06.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+# this job script is specifical for step 05 of the pipeline
+
+#SBATCH -A naiss2023-22-58
+#SBATCH -p node -N 5
+#SBATCH -t 02:00:00
+#SBATCH -J job-06-ND-pipeline
+
+BASE_DIR=/proj/naiss2023-22-58
+SIF_FILE=$BASE_DIR/ND-eval-pipeline/NDeval-pipeline-rackham-with-stdout-redirect-new.sif
+SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
+CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Cr52-mod-cov-func-try4.R
+
+module load openmpi/4.0.2
+mpirun -np 100 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/12_create_randomfiles_mod_cov_func.R $CONFIG_FILE
---
+++ submit-slurm-jobs-Fe56.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+
+# submit a series of slurm jobs to perform the different steps in the pipeline, when one
+# job depends on the results of another, make sure that they are executed in the correct
+# sequence using the dependency flag of sbatch
+
+# JOBID_01=$(sbatch Fe56-job-01.sh) &&
+# JOBID_02=$(sbatch --dependency=afterok:${JOBID_01##* } Fe56-job-02.sh) &&
+# JOBID_03=$(sbatch --dependency=afterok:${JOBID_02##* } Fe56-job-03.sh) &&
+# JOBID_04=$(sbatch --dependency=afterok:${JOBID_03##* } Fe56-job-04.sh) &&
+# JOBID_05=$(sbatch --dependency=afterok:${JOBID_04##* } Fe56-job-05.sh) &&
+# JOBID_06=$(sbatch --dependency=afterok:${JOBID_05##* } Fe56-job-06.sh)
+# 
+# echo $JOBID_01
+# echo $JOBID_02
+# echo $JOBID_03
+# echo $JOBID_04
+# echo $JOBID_05
+# echo $JOBID_06
+
+#############################
+
+# JOBID_04=$(sbatch Fe56-job-04.sh) &&
+# JOBID_05=$(sbatch --dependency=afterok:${JOBID_04##* } Fe56-job-05.sh) &&
+# JOBID_06=$(sbatch --dependency=afterok:${JOBID_05##* } Fe56-job-06.sh)
+# 
+# echo $JOBID_04
+# echo $JOBID_05
+# echo $JOBID_06
+
+#############################
+
+JOBID_03=$(sbatch Fe56-job-03.sh) &&
+JOBID_04=$(sbatch --dependency=afterok:${JOBID_03##* } Fe56-job-04.sh) &&
+JOBID_05=$(sbatch --dependency=afterok:${JOBID_04##* } Fe56-job-05.sh) &&
+JOBID_06=$(sbatch --dependency=afterok:${JOBID_05##* } Fe56-job-06.sh)
+
+echo $JOBID_03
+echo $JOBID_04
+echo $JOBID_05
+echo $JOBID_06
