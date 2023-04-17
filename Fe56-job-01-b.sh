93fff5e6b4306909045e178e5f582721ecbebf48
alfgook <alg.gook@physics.uu.se>
2023-04-17 16:18:01 +0200

    added Fe56 config file

--- Fe56-job-01-b.sh
+++ Fe56-job-01-b.sh
@@ -7,7 +7,7 @@
 
 #SBATCH -A naiss2023-22-58
 #SBATCH -p core -n 20
-#SBATCH -t 1:00
+#SBATCH -t 3:00
 #SBATCH -J job-01-ND-pipeline
 
 BASE_DIR=/proj/naiss2023-22-58
@@ -16,7 +16,7 @@ SCRIPT_DIR=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script-Cr
 SCRIPT_DIR_ALT=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/script
 CONFIG_FILE=$BASE_DIR/ND-eval-pipeline/eval-fe56-scripts/config/config-Fe56.R
 
-apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties_alt.R $CONFIG_FILE
+#apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/03_extract_experimental_uncertainties_alt.R $CONFIG_FILE
 
 apptainer exec --bind $BASE_DIR $SIF_FILE Rscript --vanilla $SCRIPT_DIR/04_correct_stat_unc.R $CONFIG_FILE
 
